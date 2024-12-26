3b220707eaf854820b47ccd9b329eade17ffbccd
Vinícius Hoyer <contact@vhoyer.dev>
2024-12-26 18:03:28 -0300

    ✨ Add chip theme variation

--- stages/_debug/battle/queue_icon.gd
+++ stages/_debug/battle/queue_icon.gd
@@ -9,6 +9,15 @@ extends PanelContainer
 		mage.updated.connect(update_labels)
 		update_labels()
 
+@export var is_current: bool = false:
+	set(value):
+		is_current = value
+		if is_current:
+			var active = self.get_theme_stylebox("active", self.theme_type_variation)
+			self.add_theme_stylebox_override("panel", active)
+		else:
+			self.remove_theme_stylebox_override("panel")
+
 func _ready() -> void:
 	update_labels()
 
