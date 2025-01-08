f6f6f600940398205975e071b4ee9197dab3b38d
Vinícius Hoyer <contact@vhoyer.dev>
2025-01-08 20:00:00 -0300

    🐛 Properly disconnect events from setup screen

--- stages/_debug/setup/setup.gd
+++ stages/_debug/setup/setup.gd
@@ -18,12 +18,17 @@ func _ready() -> void:
 	party_setup(&'party_a', party_a)
 	party_setup(&'party_b', party_b)
 
+
 func party_setup(side: StringName, party: Party) -> void:
-	party.updated.connect(SceneManager.set_payload.bind(side, party))
+	var set_payload = SceneManager.set_payload.bind(side, party)
+	
+	party.updated.connect(set_payload)
 	SceneManager.set_payload(side, party)
+	
 	(get_node("%%%s" % [side]) as PartyDisplay).set_party(party)
 	(get_node("%%grimoire_%s" % [side]) as Button).button_down.connect(show_grimoires_popup.bind(party))
-
+	
+	self.tree_exiting.connect(func(): party.updated.disconnect(set_payload))
 
 func show_grimoires_popup(party: Party) -> void:
 	var instance = GrimoiresScene.instantiate()
